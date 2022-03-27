359c2d6097e6a9771b142a649ace9d71557c15b1
kalam@git143 <magnitiait@gmail.com>
2022-03-27 13:10:11 +0530

    dfhh fhj

--- src/test/java/mypack/Test1.java
+++ src/test/java/mypack/Test1.java
@@ -7,8 +7,8 @@ public class Test1
 		int a=10;
 		int b=20;
 		System.out.println("Addition is "+(a+b));
-		System.out.println("Subtraction is"+(a-b));
-		System.out.println(a*b);
-		System.out.println(a/b);
+		System.out.println("Subtraction is "+(a-b));
+		System.out.println("multiplication is "+(a*b));
+		System.out.println("divison is "+(a/b));
 	}
 }
